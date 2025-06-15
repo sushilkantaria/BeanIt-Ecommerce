9d2bfbd02323a9fc1e9e6f65dda05df5ec32e9fc
sushilkantaria <124977763+sushilkantaria@users.noreply.github.com>
2025-06-15 10:20:34 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -44,7 +44,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product', image);
 
-        await fetch('http://localhost:4000/upload', {
+        await fetch('https://beanit-ecommerce-backend.onrender.com/upload', {
             method: 'POST',
             headers: {
                 Accept: 'application/json'
@@ -55,7 +55,7 @@ const AddProduct = () => {
         if (responseData.success) {
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct', {
+            await fetch('https://beanit-ecommerce-backend.onrender.com/addproduct', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
